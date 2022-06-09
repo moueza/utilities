348b7a0ba89e3f8d5324ed3e618f4d37d2db62a3
moueza <221372+moueza@users.noreply.github.com>
2022-06-09 09:24:53 +0200

    Update versioning.sh

---
+++ versioning.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#alpha : see version.txt ... get number
+#beta : search v = in files under .
+#delta : get top and bottom around
+#eps : concatenate top+v = version + bottom for each file
